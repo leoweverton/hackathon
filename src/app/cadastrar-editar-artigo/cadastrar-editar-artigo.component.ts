d07af1244032c2381d60e7a7e1fb06aa85faf164
Léo Nunes Dos Santos <leo.weverton.santos@gmail.com>
2017-07-22 15:37:04 -0300

    commit inicial

---
+++ src/app/cadastrar-editar-artigo/cadastrar-editar-artigo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-editar-artigo',
+  templateUrl: './cadastrar-editar-artigo.component.html',
+  styleUrls: ['./cadastrar-editar-artigo.component.css']
+})
+export class CadastrarEditarArtigoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
